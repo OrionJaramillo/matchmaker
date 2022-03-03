9788d36612467fdaea88d698ead65ae50c8cdda9
OrionJaramillo <99500600+OrionJaramillo@users.noreply.github.com>
2022-03-03 14:22:09 -0700

    Preliminary Front-End built
    Version 1 of the front-end is built

---
+++ client/src/components/ChatDisplay.js
@@ -0,0 +1,15 @@
+import Chat from "./Chat"
+import ChatInput from "./ChatInput"
+
+
+const ChatDisplay = () => {
+    return(
+        <>
+
+        <Chat />
+        <ChatInput />
+        </>
+    )
+}
+
+export default ChatDisplay
